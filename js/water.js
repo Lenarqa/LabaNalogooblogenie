ef7ac02df952e74f3c1cab7b7cce0f8639773bd8
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2020-09-20 23:32:59 +0700

    add stavka arrays

--- js/water.js
+++ js/water.js
@@ -1,13 +1,111 @@
-function hello() {
-    let regionValue = document.getElementById('region');
-    let pool = document.getElementById('pool');
+let waterPointValue = document.getElementById("waterPoint").value;
+let regionValue = document.getElementById('region').value;
+let pool = document.getElementById('pool');
+let nalogStavka = [
+    [//Северный
+        [300, 384],//Волга.
+        [264, 348],//Нева.
+        [246, 300],//Печора.
+        [258, 312],//Северная Двина.
+        [306, 378],//Прочие реки и озера.
+    ],
+    [//Северно-Западный 
+        [294, 390],//Волга.
+        [288, 366],//Западная Двина.
+        [258, 342],//Нева.
+        [282, 372],//Прочие реки и озера.
+    ],
+    [//Центральный
+        [288, 360],//Волга.
+        [276, 342],//Днепр.
+        [294, 384],//Дон.
+        [306, 354],//Западная Двина.
+        [252, 306],//Нева.
+        [264, 336],//Прочие реки и озера.
+    ],
+    [//Волго-Вятский
+        [282, 336],//Волга.
+        [252, 312],//Северная Двина.
+        [270, 330],//Прочие реки и озера.
+    ],
+    [//Центально-Черноземный
+        [258, 318],//Днепр.
+        [336, 402],//Дон.
+        [282, 354],//Волга.
+        [258, 318],//Прочие реки и озера.
+    ],
+    [//Поволжский
+        [294, 348],//Волга.
+        [360, 420],//Дон.
+        [264, 342],//Прочие реки и озера.
+    ],
+    [//Северо-Кавказский
+        [390, 486],//Дон.
+        [480, 570],//Кубань.
+        [480, 576],//Самур.
+        [456, 540],//Сулак.
+        [468, 558],//Терек.
+        [540, 654],//Прочие реки и озера.
+    ],
+    [//Уральский
+        [294, 444],//Волга.
+        [282, 456],//Обь.
+        [354, 534],//Урал.
+        [306, 390],//Прочие реки и озера.
+    ],
+    [//Западно-Сибирский
+        [270, 330],//Обь.
+        [276, 342],//Прочие реки и озера.
+    ],
+    [//Восточно-Сибирский
+        [276, 330],//Амур.
+        [246, 306],//Енисей.
+        [252, 306],//Лена.
+        [264, 348],//Обь.
+        [576, 678],//Озеро Байкал.
+        [282, 342],//Прочие реки и озера.
+    ],
+    [//Дальневосточный
+        [264, 336],//Амур.
+        [288, 342],//Лена.
+        [252, 306],//Прочие реки и озера.
+    ],
+    [//Калининградская область 
+        [276, 324],//Неман.
+        [288, 336],//Прочие реки и озера.
+    ],
+]
 
+let nalogStavkaSea = [8.28, 8.4, 6.36, 14.88, 14.88, 11.52, 4.8, 4.68, 4.44, 4.32, 5.28, 5.64, 7.68, 8.04];
+
+function ChangeWaterPoint() {
+    waterPointValue = document.getElementById("waterPoint").value;
+    let itemList = document.getElementById("item-list");
+    switch (waterPointValue) {
+        case '0':
+            itemList.children[7].style.display = 'none';
+            itemList.children[4].style.display = 'flex';
+            itemList.children[5].style.display = 'flex';
+            itemList.children[6].style.display = 'flex';
+            break;
+    
+        default:
+            itemList.children[7].style.display = 'flex';
+            itemList.children[4].style.display = 'none';
+            itemList.children[5].style.display = 'none';
+            itemList.children[6].style.display = 'none';
+            break;
+    }
+}
+
+function hello() {
     if(pool.innerHTML != '') {
         pool.innerHTML = '';
     }
 
-    console.log(typeof regionValue.value);
-    switch (regionValue.value) {
+    regionValue = document.getElementById('region').value;
+
+    switch (regionValue) {
         case '0':
             pool.innerHTML =`
             <option value="0">Волга</option>
@@ -110,3 +208,8 @@ function hello() {
     }
 }
 
+function getResult() {
+   if(waterPointValue == '0') {
+        let 
+   }
+}
